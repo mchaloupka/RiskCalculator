66f654ef962564096c859e9070ee8b69137f394b
Milos Chaloupka <chaloupkamilos@hotmail.com>
2021-05-10 23:42:57 +0100

    Significant optimization

--- src/Calculator.fs
+++ src/Calculator.fs
@@ -5,52 +5,39 @@ type StateProbability = { Probability: float; Outcome: State }
 type Probabilities = StateProbability list
 
 module Calculator =
-    let probabilitiesForState state =
-        if state.Attacker < 2 || state.Defender < 1 then
-            sprintf "Cannot fight in state %A" state
-            |> invalidOp
-        
-        let rec generateThrownNumbers throwCount =
-            if throwCount = 0 then [[]]
-            else
-                throwCount - 1
-                |> generateThrownNumbers
-                |> List.collect (
-                    fun xs ->
-                        [1..6] |> List.map (fun x -> x :: xs)
-                )         
-
-        let attackerThrows =
-            if state.Attacker > 3 then 3 elif state.Attacker = 3 then 2 else 1
+    let rec private generateThrownNumbers throwCount =
+        if throwCount = 0 then [[]]
+        else
+            throwCount - 1
             |> generateThrownNumbers
+            |> List.collect (
+                fun xs ->
+                    [1..6] |> List.map (fun x -> x :: xs)
+            )
 
-        let defenderThrows =
-            if state.Defender > 1 then 2 else 1
-            |> generateThrownNumbers
-
-        let rec throwOutcome (attackerThrows, defenderThrows) =
-            let rec removeHighestThrow throws =
-                match throws with
-                | [] -> "Failed calculation" |> invalidOp
-                | [x] -> x, []
-                | x :: xs ->
-                    let (otherMax, otherXs) = removeHighestThrow xs
-                    if x > otherMax then
-                        x, otherMax::otherXs
-                    else
-                        otherMax, x::otherXs
-
-            if defenderThrows |> List.isEmpty || attackerThrows |> List.isEmpty then 0, 0
-            else
-                let (attHigh, attXs) = attackerThrows |> removeHighestThrow
-                let (defHigh, defXs) = defenderThrows |> removeHighestThrow
-                let (attO, defO) = throwOutcome (attXs, defXs)
-                if attHigh > defHigh then
-                    (attO, defO + 1)
+    let rec private throwOutcome (attackerThrows, defenderThrows) =
+        let rec removeHighestThrow throws =
+            match throws with
+            | [] -> "Failed calculation" |> invalidOp
+            | [x] -> x, []
+            | x :: xs ->
+                let (otherMax, otherXs) = removeHighestThrow xs
+                if x > otherMax then
+                    x, otherMax::otherXs
                 else
-                    (attO + 1, defO)
+                    otherMax, x::otherXs
 
-        let aggregateStates outputs =
+        if defenderThrows |> List.isEmpty || attackerThrows |> List.isEmpty then 0, 0
+        else
+            let (attHigh, attXs) = attackerThrows |> removeHighestThrow
+            let (defHigh, defXs) = defenderThrows |> removeHighestThrow
+            let (attO, defO) = throwOutcome (attXs, defXs)
+            if attHigh > defHigh then
+                (attO, defO + 1)
+            else
+                (attO + 1, defO)
+
+    let private aggregateStates outputs =
             let rec aggregate output toProcess =
                 match toProcess with
                 | (state, probability) :: xs ->
@@ -85,13 +72,51 @@ module Calculator =
                         fun (defender, probability) ->
                             { Outcome = { Attacker = attacker; Defender = defender }; Probability = probability }
                     )
-            )   
+            )
+
+    let private generatePossibleOutcome attackerThrowCounts defenderThrowCounts =
+        let attackerThrows = attackerThrowCounts |> generateThrownNumbers
+        let defenderThrows = defenderThrowCounts |> generateThrownNumbers
 
         attackerThrows
         |> List.collect (fun a -> defenderThrows |> List.map (fun d -> a, d))
-        |> List.map (throwOutcome >> (fun (a,d) -> { Attacker = state.Attacker - a; Defender = state.Defender - d }))
+        |> List.map (throwOutcome >> (fun (a,d) -> { Attacker = a; Defender = d }))
         |> fun x ->
             let size = x |> List.length |> float
             let probability = 1.0 / size
             x |> List.map (fun x -> x, probability)
         |> aggregateStates
+
+    let private generatePossibleOutcomeForAttacker attackerThrowCounts =
+        Map.empty
+        |> Map.add 2 (generatePossibleOutcome attackerThrowCounts 2)
+        |> Map.add 1 (generatePossibleOutcome attackerThrowCounts 1)
+
+    let private possibleOutcomes = 
+        Map.empty
+        |> Map.add 3 (generatePossibleOutcomeForAttacker 3)
+        |> Map.add 2 (generatePossibleOutcomeForAttacker 2)
+        |> Map.add 1 (generatePossibleOutcomeForAttacker 1)
+
+    let probabilitiesForState state =
+        if state.Attacker < 2 || state.Defender < 1 then
+            sprintf "Cannot fight in state %A" state
+            |> invalidOp
+
+        let attackerThrows =
+            if state.Attacker > 3 then 3 elif state.Attacker = 3 then 2 else 1
+
+        let defenderThrows =
+            if state.Defender > 1 then 2 else 1
+
+        possibleOutcomes.[attackerThrows].[defenderThrows]
+        |> List.map (
+            fun probability ->
+                { 
+                    Outcome = { 
+                        Attacker = state.Attacker - probability.Outcome.Attacker
+                        Defender = state.Defender - probability.Outcome.Defender 
+                    }
+                    Probability = probability.Probability
+                }
+        )
